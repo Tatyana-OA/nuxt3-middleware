370d7ab8ee28840d29cc858417b3c39cebfa827b
TAsenova <tasenova@mentormate.com>
2024-10-08 19:01:35 +0300

    Error handling in the middleware

--- middleware/1A.setup.global.js
+++ middleware/1A.setup.global.js
@@ -1,3 +1,18 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   console.log("----Setup Middleware----");
+
+  if (to.path === "/login") {
+    throw createError({
+      statusCode: 405,
+      statusMessage:
+        "Oh, snap, I'm learning about middleware errors, so there you go.",
+    });
+  }
+
+  // try {
+  //   const data = await fetch("/api/this-path");
+  //   return navigateTo("/profile" + data.user.name);
+  // } catch (err) {
+  //   return abortNavigation(err);
+  // }
 });
